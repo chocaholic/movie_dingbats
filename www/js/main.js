fa2ba7bbf5b8434061ff03ef44e467800c364b6f
Mags Hennessy <mags@woden.ie>
2017-05-11 11:47:45 +0100

    Save different levels

--- www/js/main.js
+++ www/js/main.js
@@ -1,30 +1,71 @@
-var lvl = 1;
 var cat;
-
+//var save = JSON.parse(localStorage.getItem("save"));
+var lvl;
 $(document).ready(function () {
-    if(window.location.href.indexOf("index.html") == -1) {
+    
+    //if url is template.html
+    if (window.location.href.indexOf("template.html") > -1) {
+        console.log(localStorage);
+        
+        cat = localStorage.getItem("cat");
+    
+        console.log(cat);
+        
+        var saveCat;
+        switch (cat) {
+        case "1":
+            saveCat = JSON.parse(localStorage.getItem("saveCat1"));
+            checkSave(saveCat);
+            break;
+        case "2":
+            saveCat = JSON.parse(localStorage.getItem("saveCat2"));
+            checkSave(saveCat);
+            break;
+    }
+        
+//        if (save === null) {
+//            lvl = 1;
+//        }
+//        else {
+//            lvl = save["lvl"];
+//        }
         checkLevel();
     }
-    else {
+    //if url is index.html
+    else if (window.location.href.indexOf("index.html") > -1) {
         localStorage.removeItem("cat");
     }
 });
 
+function checkSave(saveCat) {
+    if (saveCat === null) {
+        lvl = 1;
+    }
+    else {
+        lvl = saveCat["lvl"];
+    }
+}
+
 function checkLevel() {
-    var levelArray;
-    cat = localStorage.getItem("cat");
+    var jsonLvl;
+    
+    //Load the right questions to the current category
     switch (cat) {
         case "1":
-            levelArray = jsonObject.LevelQ1;
+            jsonLvl = jsonObject.Cat1Q;
             break;
         case "2":
-            levelArray = jsonObject.LevelQ2;
+            jsonLvl = jsonObject.Cat2Q;
             break;
     }
     
-    if (lvl <= levelArray.length) {
-        var levelQ = levelArray[lvl - 1];
-        document.getElementById("movieimg").innerHTML = '<img src=' + Object.values(levelQ) + ' />';
+    console.log(lvl, jsonLvl.length);
+    
+    //Load img question
+    if (lvl <= jsonLvl.length) {
+        var question = jsonLvl[lvl - 1];
+        console.log(question);
+        document.getElementById("movieimg").innerHTML = '<img src=' + Object.values(question) + ' />';
     }
     else {
         $('#endlevel').modal('show');
@@ -33,42 +74,80 @@ function checkLevel() {
 }
 
 $(".animLvl").click(function (event) {
-    window.location = "animated.html";
+    loadTemplate();
     localStorage.setItem("cat", 1);
 });
 
 $(".actionLvl").click(function (event) {
-    window.location = "action.html"; 
+    loadTemplate();
     localStorage.setItem("cat", 2);
 });
+
+$(".resetlvl").click(function (event) {
+    localStorage.clear();
+//    lvl = 1;
+    
+    console.log(localStorage);
+});
+
+function loadTemplate() {
+    window.location = "template.html";
+}
 
 $(".ansbtn").click(function (event) {
     event.preventDefault();
 
-    var answer = $("#ans").val();
-    answer = answer.toLowerCase().replace(/\s+/g, '');
+    //get user answer and strip white space and change to lowercase
+    var userAns = $("#ans").val();
+    userAns = userAns.toLowerCase().replace(/\s+/g, '');
     
-    var answerArray;
+    var jsonAns;
+    
+    //Load the right userAnss to the current category
     switch (cat) {
         case "1":
-            answerArray = jsonObject.LevelA1;
+            jsonAns = jsonObject.Cat1Ans;
             break;
         case "2":
-            answerArray = jsonObject.LevelA2;
+            jsonAns = jsonObject.Cat2Ans;
             break;
     }
 
-    var result = answerArray[lvl - 1];
+    var rightAns = jsonAns[lvl - 1];
+    console.log(rightAns);
 
-    if (answer == Object.values(result)) {
+    //Check user answer matches right answer
+    if (userAns == Object.values(rightAns)) {
         $("#ans").val('');
         $(".wrong").hide();
         $('#correctAns').modal('show');
+        
+        //Save user progress
+        saveProgress();
     }
     else {
         $(".wrong").show();
     }
 });
+
+function saveProgress(){
+    
+    var saveProgress = {
+        "cat": cat,
+        "lvl": lvl + 1
+    };
+    
+    switch (cat) {
+        case "1":
+            localStorage.setItem("saveCat1", JSON.stringify(saveProgress));
+            break;
+        case "2":
+            localStorage.setItem("saveCat2", JSON.stringify(saveProgress));
+            break;
+    }
+    
+    console.log(localStorage);    
+}
 
 $(".next").click(function (event) {
     event.preventDefault();
